6343828cc0fe97bed39ca05b9d22abaef6d7796b
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-10-12 22:13:23 -0400

    working on generating random colors

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
-/**
- * Created by Gabriele on 10/12/16.
- */
+var colors = generateRandomColors(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+
+    squares[i].addEventListener("click", function() {
+        //grab color of clicked square
+        var clickedColor = this.style.background;
+        //compare color to picked color
+        if(clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct";
+            changeColors(clickedColor);
+        } else {
+            this.style.background = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
+    });
+}
+
+function changeColors(color) {
+    // loop through all squares
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].style.background = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num) {
+    // Make array
+    var arr = [];
+    // Add num random colors to arr
+    for(var i = 0; i < num; i++) {
+        // get random color and push into array
+    }
+    // return arr
+    return arr;
+}
+
+function randomColor() {
+
+}
